793c08c1844b872c08ba13f90fb50f26341f42d1
GH Cao <driver1998.ms@outlook.com>
2022-01-16 18:38:16 +0800

    Update the binexport scripts for the new ARM64 BSP

--- tools/binexport.cmd
+++ tools/binexport.cmd
@@ -3,15 +3,19 @@ REM Export Script for exporting required binary files only
 goto START
 
 :Usage
-echo Usage: binexport [Release]/[Debug] [TargetDir]
+echo Usage: binexport [RPi]/[RPi3.ARM64]/[RPi4.ARM64] [Release]/[Debug] [TargetDir]
+echo    RPi....................... Export the Raspberry 2/3 32Bit BSP.
+echo    RPi3.ARM64................ Export the Raspberry 3   64Bit BSP.
+echo    RPi4.ARM64................ Export the Raspberry 4   64Bit BSP.
+echo        One of the above should be specified
 echo    Release................... Picks the release binaries
 echo    Debug..................... Picks the debug binaries
 echo        One of the above should be specified
 echo    [TargetDir]............... Directory to export the files.
 echo    [/?]...................... Displays this usage string.
 echo    Example:
-echo        binexport Release C:\bsp_v1.0_release
-echo        binexport Debug C:\bsp_v1.0_debug
+echo        binexport RPi Release C:\bsp_v1.0_release
+echo        binexport RPi4.ARM64 Debug C:\bsp_v1.0_debug
 
 
 exit /b 1
@@ -22,11 +26,26 @@ REM Input validation
 if [%1] == [/?] goto Usage
 if [%1] == [-?] goto Usage
 if [%1] == [] goto Usage
-if [%2] == [] (
+if [%1] == [] (
     goto Usage
 ) else (
-    set TDIR=%2\RPi
+    set PLAT=%1
 )
+if [%3] == [] (
+    goto Usage
+) else (
+    set TDIR=%3
+)
+
+REM Detect architecture
+set ARCH=INVALID
+if /I [%PLAT%] == [RPi]        set ARCH=ARM
+if /I [%PLAT%] == [RPi3.ARM64] set ARCH=ARM64
+if /I [%PLAT%] == [RPi4.ARM64] set ARCH=ARM64
+if /I [%ARCH%] == [INVALID] (
+    goto Usage
+)
+set TDIR=%3\%PLAT%
 
 pushd
 setlocal ENABLEDELAYEDEXPANSION
@@ -38,23 +57,31 @@ set REPO_BUILD_TOOL=%~dp0
 cd /d "%REPO_BUILD_TOOL%.."
 set REPO_SOURCE_ROOT=%cd%\
 
-set OUTPUT_DIR=%REPO_SOURCE_ROOT%\build\bcm2836\ARM
-set BINTYPE=%1
+set OUTPUT_DIR=%REPO_SOURCE_ROOT%\build\bcm2836\%ARCH%
+set BINTYPE=%2
 
+REM Check firmware for ARM64 platform
+if /I [%ARCH%] == [ARM64] (
+    if not exist %REPO_SOURCE_ROOT%\bspfiles\%PLAT%\Packages\RPi.BootFirmware\RPI_EFI.fd (
+        echo Error: UEFI Firmware for platform %PLAT% not found.
+        echo Please refer to windows-drivers repo on how to get it.
+        exit /b
+    )
+)
 
 if not exist %TDIR% ( mkdir %TDIR% )
-if not exist %OUTPUT_DIR%\%BINTYPE% (
+if not exist %OUTPUT_DIR%\%BINTYPE%\Output (
     echo %BINTYPE% directory not found. Do %BINTYPE% build
     goto usage
 )
 REM Copy the bspfiles
-xcopy /E /I %REPO_SOURCE_ROOT%\bspfiles\*.* %TDIR% >nul
+xcopy /E /I %REPO_SOURCE_ROOT%\bspfiles\%PLAT%\*.* %TDIR% >nul
 set DRVDIR=%TDIR%\Packages\RPi.Drivers
 
 REM Export the built binaries
-copy %OUTPUT_DIR%\%BINTYPE%\*.inf %DRVDIR% > nul
-copy %OUTPUT_DIR%\%BINTYPE%\*.sys %DRVDIR% > nul
-copy %OUTPUT_DIR%\%BINTYPE%\*.dll %DRVDIR% > nul
+copy %OUTPUT_DIR%\%BINTYPE%\Output\*.inf %DRVDIR% > nul
+copy %OUTPUT_DIR%\%BINTYPE%\Output\*.sys %DRVDIR% > nul
+copy %OUTPUT_DIR%\%BINTYPE%\Output\*.dll %DRVDIR% > nul
 
 popd
 exit /b
